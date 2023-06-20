c64d990a47fbf08d721779f9166dfde5ea5aa7c3
Mindru-Bogdan <77194184+Mindru-Bogdan@users.noreply.github.com>
2023-06-20 13:10:49 +0300

    Device Friendly Solution

--- src/Components/Submit.js
+++ src/Components/Submit.js
@@ -1,10 +1,39 @@
+import { useState } from 'react';
+
 import classes from './Submit.module.css';
 
 const Submit = () => {
+  const [email, setEmail] = useState('');
+  const [validity, setValidity] = useState(true);
+
+  const emailSet = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const isEmailValid = (email) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
+  const ValidityHandler = (event) => {
+    if (!isEmailValid(email)) {
+      setValidity(false);
+    } else {
+      setValidity(true);
+    }
+  };
+
   return (
     <div>
-      <input type="text" placeholder="Your email address..."></input>
-      <p className={classes.button}>Notify Me</p>
+      <input
+        type="text"
+        className={validity ? classes.normalInput : classes.errorInput}
+        placeholder="Your email address..."
+        onChange={emailSet}
+      ></input>
+      <p className={validity ? classes.hidden : classes.errorText}>Please provide a valid email address</p>
+      <p className={classes.button} onClick={ValidityHandler}>
+        Notify Me
+      </p>
     </div>
   );
 };
